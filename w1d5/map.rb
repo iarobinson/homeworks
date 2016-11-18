542017446d3c6abb4d44744d598eabea2328c500
Ian <ian (at) ianrobinson.net>
2016-11-18 07:17:09 -0800

    Initialize map.rb

---
+++ w1d5/map.rb
@@ -0,0 +1,28 @@
+class Map
+  def initialize
+    @el = el
+  end
+
+  def assign(key, value)
+    if lookup(key)
+      @value =
+    else
+
+
+  end
+
+  def lookup(key)
+
+  end
+
+  def remove(key)
+
+  end
+
+  def show
+
+  end
+end
+
+m = Map.new
+m.assign("a", 5)
