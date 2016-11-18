a80bca15bd95f5158c555e19e59b5cc1027a7c8a
Ian <ian (at) ianrobinson.net>
2016-11-18 06:58:19 -0800

    Add test cases

--- w1d5/stack.rb
+++ w1d5/stack.rb
@@ -16,3 +16,13 @@ class Stack
     @stack.dup # My initial thought was = p @stack
   end
 end
+
+s = Stack.new
+s.add("i")
+s.add("a")
+s.add("p")
+s.add(1)
+s.remove
+s.show # What does s.show do? Duplicates, sure... but why?
+s.add("n")
+p s
