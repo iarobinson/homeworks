fa1f4aa0134ab1412fb13b673140a0b6d0545245
Ian <ian (at) ianrobinson.net>
2016-11-18 06:45:39 -0800

    Initialize stack class homeworkd

---
+++ w1d5/stack.rb
@@ -0,0 +1,17 @@
+class Stack
+  def initialize
+    # ivar stack here
+  end
+
+  def add(el)
+    # add element to stack
+  end
+
+  def remove
+    # remove an element from stack
+  end
+
+  def show
+    # return a copy to stack
+  end
+end
