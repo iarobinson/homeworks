5e0fa2ebf3258d6e71761df2ae85300c902691d8
Ian <ian (at) ianrobinson.net>
2016-11-18 07:06:25 -0800

    Add test case to queue and correct shift/unshift error on line 8

--- w1d5/queue.rb
+++ w1d5/queue.rb
@@ -5,7 +5,7 @@ class Queue
   end
 
   def enqueue(el)
-    @stack.shift(el)
+    @stack.unshift(el)
   end
 
   def dequeue
@@ -16,3 +16,13 @@ class Queue
     @stack.dup
   end
 end
+
+q = Queue.new
+q.enqueue(1)
+q.enqueue(2)
+q.dequeue
+q.enqueue(3)
+p q
+
+# This shows the pattern of this LIFO system. 1 is removed while 2
+#   and three remain.
