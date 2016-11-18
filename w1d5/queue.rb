29745890264f124a8a45015bb05310506beb9a98
Ian <ian (at) ianrobinson.net>
2016-11-18 07:00:56 -0800

    Add queue.rb, a FIFO stack class

---
+++ w1d5/queue.rb
@@ -0,0 +1,18 @@
+
+class Queue
+  def initialize
+    # Code Here
+  end
+
+  def enqueue(el)
+    # Code Here
+  end
+
+  def dequeue
+    # Code Here
+  end
+
+  def show
+    # Code Here
+  end
+end
