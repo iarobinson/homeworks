8c249f10e986b1c2b02abcf8248f825bf6a5f896
Ian <ian (at) ianrobinson.net>
2016-11-18 08:54:34 -0800

    Write initialize, assign and lookup to map.rb

--- w1d5/map.rb
+++ w1d5/map.rb
@@ -1,28 +1,33 @@
 class Map
   def initialize
-    @el = el
+    @map = []
   end
 
   def assign(key, value)
-    if lookup(key)
-      @value =
-    else
-
-
+    pair_index = @map.index { |pair| pair[0] == key }
+    pair_index ? @map[pair_index][1] = value : @map.push([key, value])
+    [key, value]
   end
 
   def lookup(key)
+    @map.each do |pair|
+      if pair[0] == key
+        return pair[1]
+      end
+    end
 
+    nil
   end
 
   def remove(key)
-
+    #
   end
 
   def show
-
+    #
   end
 end
 
 m = Map.new
-m.assign("a", 5)
+m.assign(:hi, 2)
+# p m.lookup(:hi)
