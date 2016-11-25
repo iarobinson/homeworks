5f64a29a2e58c6ea86bd2469cb0eaccd06a36e5b
Ian <ian (at) ianrobinson.net>
2016-11-25 07:20:46 -0800

    Add self-directed solutions to lru_cache.rb

--- w2d5/lru_cache.rb
+++ w2d5/lru_cache.rb
@@ -1,20 +1,72 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class LRUCache
-  def initialize
+  def initialize(length)
+    @length = length
+    @cache = Array.new
   end
 
   def count
     # returns number of elements currently in cache
+    @length
   end
 
   def add(el)
     # adds element to cache according to LRU principle
+    if @cache.length < @length
+      @cache += [el]
+    elsif @cache.include?(el)
+      @cache -= [el]
+      @cache += [el]
+    else
+      @cache.shift
+      @cache += [el]
+    end
   end
 
   def show
     # shows the items in the cache, with the LRU item first
+    p @cache
   end
 
   private
   # helper methods go here!
 
 end
+
+
+
+johnny_cache = LRUCache.new(4)
+
+johnny_cache.add("I walk the line")
+johnny_cache.add(5)
+
+p johnny_cache.count # => returns 2
+
+johnny_cache.add([1,2,3])
+johnny_cache.add(5)
+johnny_cache.show
+johnny_cache.add(-5)
+johnny_cache.add({a: 1, b: 2, c: 3})
+johnny_cache.add([1,2,3,4])
+johnny_cache.add("I walk the line")
+johnny_cache.add(:ring_of_fire)
+johnny_cache.add("I walk the line")
+johnny_cache.add({a: 1, b: 2, c: 3})
+
+p johnny_cache
