748774833f6706472fdeb1b5b5fdeba3d05476bc
Ian <ian (at) ianrobinson.net>
2016-11-25 07:26:09 -0800

    Add notes on improvements after viewing solutions

--- w2d5/lru_cache.rb
+++ w2d5/lru_cache.rb
@@ -1,20 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class LRUCache
   def initialize(length)
     @length = length
@@ -28,35 +11,34 @@ class LRUCache
 
   def add(el)
     # adds element to cache according to LRU principle
-    if @cache.length < @length
-      @cache += [el]
+    if @cache.length < @length # Should be swapped with below elsif else early duplicates be recorded in lieu of important info
+      @cache << el # It was very early, I used += [] in lieu of the shovel... very strange
     elsif @cache.include?(el)
-      @cache -= [el]
-      @cache += [el]
+      @cache.delete(el)
+      @cache << el
     else
       @cache.shift
-      @cache += [el]
+      @cache << el
     end
   end
 
   def show
     # shows the items in the cache, with the LRU item first
     p @cache
+    nil # So the method returns... I guess this is important
   end
 
-  private
-  # helper methods go here!
+  # Should have deleted private as it wasn't important
 
 end
 
-
-
+# For Testing
 johnny_cache = LRUCache.new(4)
 
 johnny_cache.add("I walk the line")
 johnny_cache.add(5)
 
-p johnny_cache.count # => returns 2
+# johnny_cache.count # => returns 2
 
 johnny_cache.add([1,2,3])
 johnny_cache.add(5)
@@ -69,4 +51,4 @@ johnny_cache.add(:ring_of_fire)
 johnny_cache.add("I walk the line")
 johnny_cache.add({a: 1, b: 2, c: 3})
 
-p johnny_cache
+johnny_cache.show
